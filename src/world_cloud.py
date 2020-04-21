8d8718fa60011270f8a57dec35308a7cd6305c2b
Dorian Vinai <dorian.vinai@epita.fr>
2020-04-21 19:54:49 +0200

    Fixed word cloud blacklist

--- src/world_cloud.py
+++ src/world_cloud.py
@@ -6,12 +6,27 @@ def world_cloud(str):
     if type(str) is list:
         str = " ".join(str)
 
-    determinant = ["le","la","un","une","de","du","des","en","dans","pour","par"]
-    for d in determinant:
-        str.replace(d,"")
+    ignored = [ ".", ",", "!", "?", ";" ]
+    for e in ignored:
+        str = str.replace(e, "")
 
+    str = str.split(" ");
+    str[:] = [word.capitalize() for word in str]
+    str = " ".join(str)
 
-    print("Generating world cloud for:", str)
+    blacklist = [
+        "il", "elle", "on",
+        "le",  "la", "un", "une", "du", "de", "les", "des",
+        "ce", "cet", "cette", "ces", "mais", "ou", "et", "donc",
+        "que", "quoi", "qui", "où",
+        "en", "dans", "pour", "par", "à",
+        "est", "a", "fait"
+    ]
+    for e in blacklist:
+        str = str.replace(e.capitalize(), "")
+
+    str = str.replace("-", "_")
+
     wordcloud = WordCloud().generate(str)
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis("off")
