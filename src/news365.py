ba9819687c23a1d370773df006e0192d675f2286
Antoine Coulon <antoine.coulon@epita.fr>
2021-05-10 19:44:55 +0200

    as

--- src/news365.py
+++ src/news365.py
@@ -3,8 +3,21 @@ import regions
 import world_cloud
 
 def main():
-    generate_cloud("ile-de-france")
     generate_cloud("auvergne-rhone-alpes")
+    generate_cloud("bourgogne-franche-comte")
+    generate_cloud("bretagne")
+    generate_cloud("centre-val-de-loire")
+    generate_cloud("corse")
+    generate_cloud("grand-est")
+    generate_cloud("hauts-de-france")
+    generate_cloud("ile-de-france")
+    generate_cloud("normandie")
+    generate_cloud("nouvelle-aquitaine")
+    generate_cloud("occitanie")
+    generate_cloud("provence-alpes-cote-dazur")
+    generate_cloud("pays-de-la-loire")
+    generate_cloud("outre-mer")
+
 
 
     #api.idf()
@@ -13,7 +26,7 @@ def main():
 def generate_cloud(region): 
     world_cloud.world_cloud(
         api.getToday(region)[1],
-        regions.getMask(region),
+        regions.getMask("vierge"),
         region + ".png"
     )
 
